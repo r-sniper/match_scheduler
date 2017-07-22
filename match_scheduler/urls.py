ee28921490c59eddf50ce195107bb2425c984946
Shibashis <shibashismallik@gmail.com>
2017-07-23 02:47:02 +0530

    SignIN Temporary

--- match_scheduler/urls.py
+++ match_scheduler/urls.py
@@ -4,5 +4,9 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^', include('home.urls')),
     url(r'^admin/', admin.site.urls),
+    url('', include('social_django.urls', namespace='social')),
+    url('', include('django.contrib.auth.urls', namespace='auth')),
+
+
 
 ]
